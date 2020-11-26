3438fbdda5cd9e22b224f690fb046d20b3b767aa
刘鸿亮 <lhl902@qq.com>
2020-11-26 14:12:40 +0800

    ADD 自定义排序接口

--- src/main/java/com/smec/template/controller/v1/api/StudentController.java
+++ src/main/java/com/smec/template/controller/v1/api/StudentController.java
@@ -7,6 +7,8 @@ import com.smec.template.service.StudentService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.stream.Collectors;
@@ -35,10 +37,26 @@ public class StudentController {
                         .map(StudentVo::formStudentVo)
                         .collect(Collectors.toList())
         );
-        pageVo.setPage(size);
         pageVo.setPage(page);
+        pageVo.setSize(size);
         pageVo.setTotal(pageData.getTotalElements());
         return pageVo;
+    }
+
+    @GetMapping("/students/sort")
+    public PageVo<StudentVo> listSort(@PageableDefault(page = 0, size = 20, sort = {"created"}, direction = Sort.Direction.DESC)Pageable pageable){
+        Page<Student> pageData = studentService.findAll(pageable);
+        PageVo<StudentVo> studentVoPageVo=new PageVo<>();
+        studentVoPageVo.setData(
+                pageData.getContent()
+                        .stream()
+                        .map(StudentVo::formStudentVo)
+                        .collect(Collectors.toList())
+        );
+        studentVoPageVo.setPage(pageData.getNumber());
+        studentVoPageVo.setSize(pageData.getSize());
+        studentVoPageVo.setTotal(pageData.getTotalElements());
+        return studentVoPageVo;
     }
 
     @PostMapping("/students")
